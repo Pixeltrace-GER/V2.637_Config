d1a411d59198cbc494e34b90f1ccb441d0dc5a57
Tim <72595331+Pixeltrace-GER@users.noreply.github.com>
2022-09-20 07:26:28 +0200

    Update Shellscripts

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,11 @@
 cd /home/pi/klipper_config
+git fetch
+git checkout master
+git pull
+git checkout -b patch-printer
+git merge master
 git add .
 git commit -m "Update $(date +"%Y%m%d-%H%M%S")"
 git push
+git checkout master
+git checkout -d patch-printer
