4f0908594fe7137e32dda6886e10b0d91d042ad3
PixelTrace-GER <git@mailws.de>
2022-09-20 07:22:11 +0200

    Update Shellscript

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,8 @@ git checkout master
 git pull
 git checkout -b patch-printer
 git merge master
-git commit -a -m "Update $(date +"%Y%m%d-%H%M%S")"
+git add .
+git commit -m "Update $(date +"%Y%m%d-%H%M%S")"
 git push
 git checkout master
 git checkout -d patch-printer
