b1a9cb790096d99c8effdbe58378c3170c8e147e
PixelTrace-GER <git@mailws.de>
2022-09-20 07:45:10 +0200

    change git push w/ upstream

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -2,10 +2,10 @@ cd /home/pi/klipper_config
 git fetch
 git checkout master
 git pull
-git checkout -b patch-printer
+git checkout -b patch-from-printer
 git merge master
 git add .
 git commit -m "Update $(date +"%Y%m%d-%H%M%S")"
-git push
+git push --set-upstream origin patch-from-printer
 git checkout master
 git checkout -d patch-printer
